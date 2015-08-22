382e98ef664b6516afb0f1aad739a0c28f473e7e
josesoto94 <Vader2013>
2015-08-21 22:24:01 -0600

    Commit

---
+++ Lab2Topicos/Lab2Topicos/Controllers/lab2.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Lab2Topicos.Controllers
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "lab2" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select lab2.svc or lab2.svc.cs at the Solution Explorer and start debugging.
+    public class lab2 : Ilab2
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ Lab2Topicos/Lab2Topicos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab2Topicos.MvcApplication" Language="C#" %>
